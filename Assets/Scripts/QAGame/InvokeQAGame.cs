f8eed41562be001f93fc4dd5f9f8276bc5889824
Szymon Balcerzak <szymon.balcerzak@szkolawchmurze.org>
2025-04-13 15:06:56 +0200

    .

--- Assets/Scripts/QAGame/InvokeQAGame.cs
+++ Assets/Scripts/QAGame/InvokeQAGame.cs
@@ -7,6 +7,7 @@ public class InvokeQAGame : MonoBehaviour
 {
     [SerializeField] private MainProgressBar mainProgressBar;
     private BoxCollider2D boxCollider;
+    public Telewizor Telewizor;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class InvokeQAGame : MonoBehaviour
     {
         boxCollider.enabled = true;
         Debug.Log("🎯 BoxCollider2D został włączony!");
+        Telewizor.gameObject.SetActive(true);
     }
 
     // Update is called once per frame

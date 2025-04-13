412fa2a2f3650f00be9a1014755801898bf5e4ad
Saksiu <nowywy@gmail.com>
2025-04-13 14:12:03 +0200

    adjioassdn

--- Assets/Scripts/Player/MainProgressBar.cs
+++ Assets/Scripts/Player/MainProgressBar.cs
@@ -1,23 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainProgressBar : MonoBehaviour
 {
+
+
+    [SerializeField] private Slider m_UISlider;
     public ProductivityBar[] productivityBars;
     public float maxProgress = 100f;
     public float currentProgress = 0f;
 
     public float progressMultiplier = 1f; // np. ile procent postępu przy pełnej produktywności
-
-    private Transform barTransform;
-    private Vector3 initialScale;
-    private Vector3 initialPosition;
-
-    void Start()
-    {
-        barTransform = transform;
-        initialScale = barTransform.localScale;
-        initialPosition = barTransform.localPosition;
-    }
+    
+    
 
     void Update()
     {
@@ -32,14 +27,17 @@ public class MainProgressBar : MonoBehaviour
 
         currentProgress += avgRatio * progressMultiplier * Time.deltaTime;
         currentProgress = Mathf.Clamp(currentProgress, 0, maxProgress);
+        
+        m_UISlider.value = currentProgress / maxProgress;
 
-        float ratio = currentProgress / maxProgress;
-        float newScaleX = initialScale.x * ratio;
+        //float ratio = currentProgress / maxProgress;
+        
         //if (Mathf.Approximately(newScaleX, 0))
         //    newScaleX = 1;
-        barTransform.localScale = new Vector3(newScaleX, initialScale.y, initialScale.z);
+        
+        //barTransform.localScale = new Vector3(newScaleX, initialScale.y, initialScale.z);
 
-        float deltaX = (initialScale.x - newScaleX) / 2f;
-        barTransform.localPosition = initialPosition - new Vector3(deltaX, 0, 0);
+        //float deltaX = (initialScale.x - newScaleX) / 2f;
+        //barTransform.localPosition = initialPosition - new Vector3(deltaX, 0, 0);
     }
 }
